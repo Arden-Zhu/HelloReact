e357ab6d31a2dedd13abfe8c1c6936a8912a5039
Arden Zhu <arden.zhu@lafayette148.com>
2018-03-02 14:48:39 -0500

    react.app.tests

---
+++ HelloReact/ClientApp/test/react.app.tests.tsx
@@ -0,0 +1,14 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as TestUtils from "react-addons-test-utils";
+import { ArrayView, ClickableItem } from '../components/Clickable';
+
+
+describe('ArrayView model tests', () => {
+    it('should create a new ArrayView', () => {
+        var app = new ArrayView();
+        expect(app).toBeDefined();
+        //expect(app.selectedItem.id).toBe(0);
+        //expect(app.selectedItem.displayName).toBe('none');
+    });
+});
--- HelloReact/Readme.txt
+++ HelloReact/Readme.txt
@@ -4,3 +4,5 @@ npm install react-addons-test-utils --save-dev
 npm install @types/react-addons-test-utils --save-dev
 
 copy jasmine-core to wwwroot
+add "jasmine" in tsconfig.json
+added file react.app.tests.tsx
