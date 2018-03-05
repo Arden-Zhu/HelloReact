05fdc00e545b1c1de0283187e5e23aef54ecf702
Arden Zhu <arden.zhu@lafayette148.com>
2018-03-05 10:18:29 -0500

    Show the board

--- HelloReact/ClientApp/components/NavMenu.tsx
+++ HelloReact/ClientApp/components/NavMenu.tsx
@@ -37,6 +37,11 @@ export class NavMenu extends React.Component<{}, {}> {
                                 <span className='glyphicon glyphicon-th-list'></span> Clickable
                             </NavLink>
                         </li>
+                        <li>
+                            <NavLink to={'/game'} activeClassName='active'>
+                                <span className='glyphicon glyphicon-th-list'></span> Game
+                            </NavLink>
+                        </li>
                     </ul>
                 </div>
             </div>
--- HelloReact/ClientApp/routes.tsx
+++ HelloReact/ClientApp/routes.tsx
@@ -5,10 +5,12 @@ import Home from './components/Home';
 import FetchData from './components/FetchData';
 import Counter from './components/Counter';
 import { Clickable } from './components/Clickable';
+import { Game } from './components/Game';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/counter' component={ Counter } />
     <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
     <Route path='/clickable' component={Clickable} />
+    <Route path='/game' component={Game} />
 </Layout>;
