f8227217918e04305c5279cfb1e0de386ff54f9f
Arden Zhu <arden.zhu@lafayette148.com>
2018-03-22 15:04:34 -0400

    TableLayout

--- HelloReact/ClientApp/components/CopyStyle.tsx
+++ HelloReact/ClientApp/components/CopyStyle.tsx
@@ -10,9 +10,86 @@ export class CopyStyle extends React.Component<CopyStyleProps, {}> {
         return <div>
             <h1>Copy Style</h1>
             <CopyStyleFilter />
+            <CopyStyleCommands/>
+            <CopyStyleTable />
         </div>
     }
 }
+
+class CopyStyleCommands extends React.Component<{}, {}> {
+    render() {
+        return <div>
+            <label>Target Season
+                    <SimpleSelect placeholder="Select a season" onValueChange={value => null}>
+                    <option value="PF18">PF18</option>
+                    <option value="FA18">FA18</option>
+                </SimpleSelect>
+            </label>
+            <button className="btn btn-primary btn-sm">Submit</button>
+            <button className="btn btn-primary btn-sm">Change</button>
+            <button className="btn btn-primary btn-sm">Change</button>
+        </div>
+    }
+}
+
+class CopyStyleTable extends React.Component<{}, {}> {
+    render() {
+        return <table>
+                <CopyStyleTableHeader />
+                <CopyStyleTableStyles />
+            </table>
+    }
+}
+
+class CopyStyleTableHeader extends React.Component<{}, {}> {
+    render() {
+        return <thead>
+            <tr>
+                <th>
+                    <input type="checkbox" />
+                </th>
+                <th>Style</th>
+                <th>A</th>
+                <th>B</th>
+                <th>C</th>
+                <th>Result</th>
+            </tr>
+        </thead>
+    }
+}
+
+class CopyStyleTableStyles extends React.Component<{}, {}> {
+    render() {
+        return <tr>
+            <td><input type="checkbox" /></td>
+            <td>WV372E-KV15</td>
+            <td>
+                <ul>
+                    <li>Color1</li>
+                    <li>Color2</li>
+                </ul>
+                <button className="btn btn-primary btn-sm">Change</button>
+            </td>
+            <td>
+                <button className="btn btn-primary btn-sm">Change</button>
+            </td>
+            <td>
+                <button className="btn btn-primary btn-sm">Change</button>
+            </td>
+            <td>
+                
+            </td>
+        </tr>
+    }
+}
+
+class temp extends React.Component<{}, {}> {
+    render() {
+        return <div>
+        </div>
+    }
+}
+
 
 class CopyStyleFilter extends React.Component<{}, {}> {
     public render() {
